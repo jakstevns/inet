3a1694f7b570e5868b0070d27e71ca8d2de1e1fb
Levente Mészáros <levy@omnetpp.org>
2023-10-16 14:43:36 +0200

    Examples: Added first NS3 TCP validation example for two hosts.

---
+++ examples/ns3tcp/example1.cc
@@ -0,0 +1,63 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/netanim-module.h"
+#include "ns3/tcp-socket-base.h"
+
+using namespace ns3;
+
+int main (int argc, char *argv[])
+{
+  NodeContainer nodes;
+  nodes.Create (2);
+
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+  GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
+
+  Config::SetDefault ("ns3::TcpSocketBase::Sack", BooleanValue (false));
+  Config::SetDefault ("ns3::TcpSocketBase::Timestamp", BooleanValue (false));
+  Config::SetDefault ("ns3::TcpSocketBase::WindowScaling", BooleanValue (false));
+  Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (536));
+  Config::SetDefault ("ns3::TcpSocket::InitialCwnd", UintegerValue (1));
+  Config::SetDefault ("ns3::TcpSocket::DelAckCount", UintegerValue (1));
+  Config::SetDefault ("ns3::TcpL4Protocol::SocketType", StringValue ("ns3::TcpNewReno"));
+
+  NetDeviceContainer devices;
+  devices = pointToPoint.Install (nodes);
+
+  InternetStackHelper stack;
+  stack.Install (nodes);
+
+  Ipv4AddressHelper address;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+
+  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+
+  uint16_t remotePort = 1000;
+
+  BulkSendHelper source ("ns3::TcpSocketFactory",
+                         InetSocketAddress (interfaces.GetAddress (1), remotePort));
+  source.SetAttribute ("MaxBytes", UintegerValue (1000000));
+  ApplicationContainer sourceApps = source.Install (nodes.Get (0));
+  sourceApps.Start (Seconds (0.0));
+  sourceApps.Stop (Seconds (10.0));
+
+  PacketSinkHelper sink ("ns3::TcpSocketFactory",
+                         InetSocketAddress (Ipv4Address::GetAny (), remotePort));
+  ApplicationContainer sinkApps = sink.Install (nodes.Get (1));
+  sinkApps.Start (Seconds (0.0));
+  sinkApps.Stop (Seconds (10.0));
+
+  pointToPoint.EnablePcapAll ("tcp-p2p");
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  return 0;
+}
+
